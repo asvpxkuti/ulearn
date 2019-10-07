d2666930792f8b04e5f873bd2f699d8555508f42
asvpxmani <eroppong@gmail.com>
2019-10-07 01:19:04 -0400

    added index and package

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('mongoose');
+const cors = require("cors");
+const mongoose = require('mongoose');
+const config = require('./config/properties');
+const contactController = require('./routes/contactRouter');
 
 
 const app = express();
+
+// Connect To Database (OLD CODE)
+mongoose.connect(config.database,{ useNewUrlParser: true }).then(()=>{
+    console.log('connected successfully');
+});
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Database ');
+});
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error '+err);
+});
 
 
 // Port Number
@@ -13,11 +30,20 @@ const port = process.env.PORT || 5000;
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const corsOptions = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: 'http://localhost:4200',
+    preflightContinue: false
+};
+
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':false}));
 
-
+app.use(cors(corsOptions));
+app.use('/contacts', contactController);
 
 // Index Route
 app.get('/', (req, res) => {
